077a53ae810908a0dfac1ffeca406886bfba40ca
SHENLISHA <shenlisha@kingsoft.com>
2018-09-30 16:28:31 +0800

    ls

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ var express = require('express'),
 	path = require('path'),
 	app = express(),
 	Base64 = require('js-base64').Base64,
-	babel = require('babel-core'),
 	port = 3000;
-console.log('aksk:', Base64.encode('{rds}:{d336b1c40a6c3ab7bd328574af4ec5be}'));
-
 
 var morgan = require('morgan'),
 	uuid = require('node-uuid');
@@ -29,22 +26,7 @@ app.use(express.urlencoded({
 	extended: false
 }));
 
-// app.use('/static', express.static('static'));
-app.use('/static', function(req, res, next) {
-	var filename = req.originalUrl;
-	console.log(path.join(__dirname, filename));
-
-	babel.transformFile(path.join(__dirname, filename), {
-		presets: ['es2015']
-	}, function(err, result) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(result.code);
-			res.set('Content-Type', 'application/javascript').end(result.code);
-		}
-	});
-});
+app.use('/static', express.static('static'));
 app.use('/node_modules', express.static('node_modules'));
 
 app.use(router);
